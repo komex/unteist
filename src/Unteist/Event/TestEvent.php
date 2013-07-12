b2fef0d798cec09446ea8b8413d46a3058237b77
Andrey Kolchenko <komexx@gmail.com>
2013-07-12 15:35:33 +0400

    Add Event objects.

---
+++ src/Unteist/Event/TestEvent.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * This file is a part of Unteist project.
+ *
+ * (c) Andrey Kolchenko <andrey@kolchenko.me>
+ */
+
+namespace Unteist\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class TestEvent
+ *
+ * @package Unteist\Event
+ * @author Andrey Kolchenko <andrey@kolchenko.me>
+ */
+class TestEvent extends Event
+{
+    /**
+     * @var string
+     */
+    protected $name;
+    /**
+     * @var TestCaseEvent
+     */
+    protected $test_case_event;
+    /**
+     * @var array
+     */
+    protected $depends = [];
+    /**
+     * @var array
+     */
+    protected $data_set = [];
+
+    /**
+     * @param string $name Test name
+     * @param TestCaseEvent $test_case_event
+     */
+    public function __construct($name, TestCaseEvent $test_case_event)
+    {
+        $this->name = $name;
+        $this->test_case_event = $test_case_event;
+    }
+
+    /**
+     * Get test's data set.
+     *
+     * @return array
+     */
+    public function getDataSet()
+    {
+        return $this->data_set;
+    }
+
+    /**
+     * Set test's data set.
+     *
+     * @param array $data_set
+     */
+    public function setDataSet($data_set)
+    {
+        $this->data_set = $data_set;
+    }
+
+    /**
+     * Get test depends.
+     *
+     * @return array
+     */
+    public function getDepends()
+    {
+        return $this->depends;
+    }
+
+    /**
+     * Set test's depends.
+     *
+     * @param array $depends
+     */
+    public function setDepends($depends)
+    {
+        $this->depends = $depends;
+    }
+
+    /**
+     * Get test name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get parent TestCase event.
+     *
+     * @return TestCaseEvent
+     */
+    public function getTestCaseEvent()
+    {
+        return $this->test_case_event;
+    }
+}
