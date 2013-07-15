fe96dc4c3a7d76b60a1bd65e1767015588e6e64e
Andrey Kolchenko <komexx@gmail.com>
2013-07-15 16:53:07 +0400

    TestRunner depends

--- src/Unteist/Processor/TestRunner.php
+++ src/Unteist/Processor/TestRunner.php
@@ -9,8 +9,9 @@ namespace Unteist\Processor;
 
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Unteist\Event\EventStorage;
+use Unteist\Event\TestCaseEvent;
+use Unteist\Event\TestEvent;
 use Unteist\Filter\AbstractMethodsFilter;
-use Unteist\TestCase;
 
 /**
  * Class TestRunner
@@ -29,13 +30,25 @@ class TestRunner
      */
     const TEST_DONE = 1;
     /**
+     * The test was skipped.
+     */
+    const TEST_SKIPPED = 2;
+    /**
+     * The test was failed.
+     */
+    const TEST_FAILED = 4;
+    /**
      * The test is marked as is already in stack list
      */
-    const TEST_MARKED = 2;
+    const TEST_MARKED = 8;
     /**
-     * @var TestCase
+     * @var \ReflectionClass
      */
     protected $test_case;
+    /**
+     * @var TestCaseEvent
+     */
+    protected $test_case_event;
     /**
      * @var \SplObjectStorage
      */
@@ -44,10 +57,6 @@ class TestRunner
      * @var AbstractMethodsFilter[]
      */
     protected $filters = [];
-    /**
-     * @var \SplDoublyLinkedList
-     */
-    protected $stack;
     /**
      * @var EventDispatcher
      */
@@ -72,7 +81,6 @@ class TestRunner
     {
         $this->dispatcher = $dispatcher;
         $this->tests = new \SplObjectStorage();
-        $this->stack = new \SplDoublyLinkedList();
         $this->precondition = new EventDispatcher();
         $this->global_storage = new \ArrayObject();
     }
@@ -90,13 +98,12 @@ class TestRunner
     /**
      * Setup TestCase.
      *
-     * @param TestCase $test_case
+     * @param \ReflectionClass $test_case
      */
-    public function precondition(TestCase $test_case)
+    public function precondition(\ReflectionClass $test_case)
     {
         $this->test_case = $test_case;
-        $class = new \ReflectionClass($this->test_case);
-        foreach ($class->getMethods() as $method) {
+        foreach ($this->test_case->getMethods() as $method) {
             $is_test_method = true;
             $modifiers = $this->parseDocBlock($method);
             foreach ($this->filters as $filter) {
@@ -202,13 +209,114 @@ class TestRunner
         return $this->asserts_count;
     }
 
+    /**
+     * Run TestCase.
+     *
+     * @return bool
+     */
     public function run()
     {
+        if ($this->tests->count() == 0) {
+            return false;
+        }
+        $this->test_case_event = new TestCaseEvent($this->test_case->getName());
+        $this->dispatcher->dispatch(EventStorage::EV_BEFORE_CASE, $this->test_case_event);
+        $this->precondition->dispatch(EventStorage::EV_BEFORE_CASE);
+        $this->tests->rewind();
+        while ($this->tests->valid()) {
+            /** @var \ReflectionMethod $method */
+            $method = $this->tests->current();
+            /** @var array $data */
+            $data = $this->tests->getInfo();
 
+            $this->runTest($method, $data);
+            $this->tests->next();
+        }
+        $this->precondition->dispatch(EventStorage::EV_AFTER_CASE);
+        $this->dispatcher->dispatch(EventStorage::EV_AFTER_CASE, $this->test_case_event);
+
+        return true;
     }
 
-    protected function runTest(\ReflectionMethod $test)
+    /**
+     * Run test method.
+     *
+     * @param \ReflectionMethod $method
+     * @param array $data
+     *
+     * @return bool Is this test success?
+     */
+    protected function runTest(\ReflectionMethod $method, array $data)
     {
+        $test_event = new TestEvent($method->getName(), $this->test_case_event);
+        $this->dispatcher->dispatch(EventStorage::EV_BEFORE_TEST, $test_event);
+        $this->precondition->dispatch(EventStorage::EV_BEFORE_TEST);
 
+        $modifiers = $data['modifiers'];
+        $continue = true;
+        if (!empty($modifiers['depends'])) {
+            $data['status'] = self::TEST_MARKED;
+            $this->tests->offsetSet($method, $data);
+            $depends = preg_replace('{[^\w,]}i', '', $modifiers['depends']);
+            $depends = explode(',', $depends);
+            $test_event->setDepends($depends);
+            if (!$this->resolveDependencies($depends)) {
+                $data['status'] = self::TEST_SKIPPED;
+                $this->tests->offsetSet($method, $data);
+
+                $continue = false;
+            }
+        }
+
+        if ($continue) {
+
+        }
+
+        $this->precondition->dispatch(EventStorage::EV_AFTER_TEST);
+        $this->dispatcher->dispatch(EventStorage::EV_AFTER_TEST, $test_event);
+
+        return $continue;
+    }
+
+    /**
+     * Check specified depends and run test if necessary.
+     *
+     * @param array $depends
+     *
+     * @return bool Is depends conditions OK?
+     * @throws \LogicException If found infinitive depends loop.
+     * @throws \InvalidArgumentException If depends methods not found.
+     */
+    protected function resolveDependencies(array $depends)
+    {
+        foreach ($depends as $depend) {
+            $method = $this->test_case->getMethod($depend);
+            if ($this->tests->offsetExists($method)) {
+                $data = $this->tests->offsetGet($method);
+                switch ($data['status']) {
+                    case self::TEST_DONE:
+                        break;
+                    case self::TEST_NEW:
+                        if (!$this->runTest($method, $data)) {
+                            return false;
+                        }
+                        break;
+                    case self::TEST_MARKED:
+                        throw new \LogicException(sprintf(
+                            'Found infinitive loop in depends (%s)',
+                            $this->test_case->getName()
+                        ));
+                    default:
+                        return false;
+                }
+            } else {
+                throw new \InvalidArgumentException(sprintf(
+                    'The depends method %s does not exists.',
+                    $method->getName()
+                ));
+            }
+        }
+
+        return true;
     }
 }
