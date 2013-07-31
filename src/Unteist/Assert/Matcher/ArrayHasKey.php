c3aa6c2e20120366016cb98e78bd02677394ebef
Andrey Kolchenko <komexx@gmail.com>
2013-07-31 12:54:18 +0400

    Added new ArrayHasKey matcher

---
+++ src/Unteist/Assert/Matcher/ArrayHasKey.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is a part of Unteist project.
+ *
+ * (c) Andrey Kolchenko <andrey@kolchenko.me>
+ */
+
+namespace Unteist\Assert\Matcher;
+
+
+/**
+ * Class ArrayHasKey
+ *
+ * @package Unteist\Assert\Matcher
+ * @author Andrey Kolchenko <andrey@kolchenko.me>
+ */
+class ArrayHasKey extends AbstractMatcher
+{
+    /**
+     * Matcher condition.
+     *
+     * @param array $actual
+     *
+     * @throws \InvalidArgumentException If variable is not an array
+     * @return bool
+     */
+    protected function condition($actual)
+    {
+        if (!is_array($actual)) {
+            throw new \InvalidArgumentException('Specified variable must be an array.');
+        }
+
+        return array_key_exists($this->expected, $actual);
+    }
+
+    /**
+     * Get name of matcher.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ArrayHasKey';
+    }
+}
