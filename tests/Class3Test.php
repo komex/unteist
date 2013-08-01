2a83a8f665cfadffe2f83ea889896bcf1fda8665
Andrey Kolchenko <komexx@gmail.com>
2013-08-01 16:11:23 +0400

    Color skipped tests.

---
+++ tests/Class3Test.php
@@ -0,0 +1,22 @@
+<?php
+use Unteist\Assert\Assert;
+
+/**
+ * This file is a part of Unteist project.
+ *
+ * (c) Andrey Kolchenko <komexx@gmail.com>
+ */
+
+class Class3Test extends \Unteist\TestCase
+{
+
+    public function testNo()
+    {
+
+    }
+
+    public function testOK()
+    {
+
+    }
+}
