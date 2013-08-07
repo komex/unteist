8626d6ecab3bebff0c801555b1be9615c32f1b20
Andrey Kolchenko <komexx@gmail.com>
2013-08-07 16:51:23 +0400

    Refactoring

--- src/Unteist/Configuration/ConfigurationValidator.php
+++ src/Unteist/Configuration/ConfigurationValidator.php
@@ -7,6 +7,7 @@
 
 namespace Unteist\Configuration;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -27,76 +28,127 @@ class ConfigurationValidator implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('unteist');
-        $rootNode->children()
-            ->integerNode('processes')
-            ->min(1)->max(10)
-            ->defaultValue(1);
-
-        $rootNode->children()
-            ->scalarNode('report_dir')
-            ->defaultNull()
-            ->cannotBeEmpty();
-
+        $this->configProcessesSection($rootNode);
+        $this->configReportDirSection($rootNode);
+        $this->configListenerSection($rootNode);
         $rootNode->append($this->getContextSection());
-
-        $rootNode->children()
-            ->arrayNode('listeners')->prototype('scalar')->isRequired();
-
-        $rootNode->children()
-            ->arrayNode('logger')->canBeEnabled()->children()
-            ->arrayNode('handlers')->requiresAtLeastOneElement()->defaultValue(
-                ['logger.handler.stream']
-            )->prototype('scalar');
-
         $rootNode->append($this->getFiltersSection());
-
+        $rootNode->append($this->getLoggerSection());
 
         return $treeBuilder;
     }
 
     /**
+     * Get definition of processes number.
+     *
+     * @param ArrayNodeDefinition $rootNode
+     *
+     * @return ArrayNodeDefinition
+     */
+    private function configProcessesSection(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode->children()
+            ->integerNode('processes')
+            ->min(1)->max(10)
+            ->defaultValue(1);
+
+        return $rootNode;
+    }
+
+    /**
+     * Get definition of report directory.
+     *
+     * @param ArrayNodeDefinition $rootNode
+     *
+     * @return ArrayNodeDefinition
+     */
+    private function configReportDirSection(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode->children()
+            ->scalarNode('report_dir')
+            ->defaultNull()
+            ->cannotBeEmpty();
+
+        return $rootNode;
+    }
+
+    /**
+     * Get definition of listeners.
+     *
+     * @param ArrayNodeDefinition $rootNode
+     *
+     * @return ArrayNodeDefinition
+     */
+    private function configListenerSection(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode->children()->arrayNode('listeners')->prototype('scalar')->isRequired();
+
+        return $rootNode;
+    }
+
+    /**
+     * Get section for logger.
+     *
+     * @return ArrayNodeDefinition
+     */
+    private function getLoggerSection()
+    {
+        $builder = new TreeBuilder;
+        $section = $builder->root('logger')->canBeEnabled();
+        $definition = $section->children()->arrayNode('handlers');
+        $definition->requiresAtLeastOneElement()->cannotBeEmpty()->defaultValue(['logger.handler.stream']);
+        $definition->prototype('scalar');
+
+        return $section;
+    }
+
+    /**
      * Get section for filters.
      *
-     * @return \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition
+     * @return ArrayNodeDefinition
      */
     private function getFiltersSection()
     {
         $builder = new TreeBuilder;
-        $filters = $builder->root('filters');
-        $node = $filters->addDefaultsIfNotSet()->children();
-        $node->arrayNode('class')->requiresAtLeastOneElement()->cannotBeEmpty()->defaultValue(
-            ['filter.class.base']
-        )->prototype('scalar');
-        $node->arrayNode('methods')->requiresAtLeastOneElement()->cannotBeEmpty()->defaultValue(
-            ['filter.methods.base']
-        )->prototype('scalar');
+        $section = $builder->root('filters')->addDefaultsIfNotSet();
 
-        return $filters;
+        $definition = $section->children()->arrayNode('class');
+        $definition->requiresAtLeastOneElement()->cannotBeEmpty()->defaultValue(['filter.class.base']);
+        $definition->prototype('scalar');
+
+        $definition = $section->children()->arrayNode('methods');
+        $definition->requiresAtLeastOneElement()->cannotBeEmpty()->defaultValue(['filter.methods.base']);
+        $definition->prototype('scalar');
+
+        return $section;
     }
 
     /**
      * Get section for context.
      *
-     * @return \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition
+     * @return ArrayNodeDefinition
      */
     private function getContextSection()
     {
         $builder = new TreeBuilder;
-        $context = $builder->root('context');
-        $node = $context->addDefaultsIfNotSet()->children();
-        $node->enumNode('error')->values(
-            ['strategy.fail', 'strategy.skip', 'strategy.incomplete', 'strategy.ignore']
-        )->cannotBeEmpty()->defaultValue('strategy.fail');
-        $node->enumNode('failure')->values(
-            ['strategy.fail', 'strategy.skip', 'strategy.incomplete', 'strategy.ignore']
-        )->cannotBeEmpty()->defaultValue('strategy.fail');
-        $node->enumNode('incomplete')->values(
-            ['strategy.fail', 'strategy.skip', 'strategy.incomplete', 'strategy.ignore']
-        )->cannotBeEmpty()->defaultValue('strategy.incomplete');
-        $node->enumNode('skip')->values(
-            ['strategy.fail', 'strategy.skip', 'strategy.incomplete', 'strategy.ignore']
-        )->cannotBeEmpty()->defaultValue('strategy.skip');
+        $section = $builder->root('context')->addDefaultsIfNotSet();
 
-        return $context;
+        $definition = $section->children()->enumNode('error');
+        $definition->values(['strategy.fail', 'strategy.skip', 'strategy.incomplete', 'strategy.ignore']);
+        $definition->cannotBeEmpty()->defaultValue('strategy.fail');
+
+        $definition = $section->children()->enumNode('failure');
+        $definition->values(['strategy.fail', 'strategy.skip', 'strategy.incomplete', 'strategy.ignore']);
+        $definition->cannotBeEmpty()->defaultValue('strategy.fail');
+
+        $definition = $section->children()->enumNode('incomplete');
+        $definition->values(['strategy.fail', 'strategy.skip', 'strategy.incomplete', 'strategy.ignore']);
+        $definition->cannotBeEmpty()->defaultValue('strategy.incomplete');
+
+        $definition = $section->children()->enumNode('skip');
+        $definition->values(['strategy.fail', 'strategy.skip', 'strategy.incomplete', 'strategy.ignore']);
+        $definition->cannotBeEmpty()->defaultValue('strategy.skip');
+
+        return $section;
     }
 }
