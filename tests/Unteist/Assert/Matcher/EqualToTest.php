90ff5d59dbdeec2ba82acef01ea7c65a70ab921c
Andrey Kolchenko <komexx@gmail.com>
2013-09-04 14:36:56 +0400

    Tests for EqualTo matcher

---
+++ tests/Unteist/Assert/Matcher/EqualToTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file is a part of unteist project.
+ *
+ * (c) Andrey Kolchenko <andrey@kolchenko.me>
+ */
+
+namespace Tests\Unteist\Assert\Matcher;
+
+use Unteist\Assert\Matcher\EqualTo;
+
+/**
+ * Class EqualToTest
+ *
+ * @package Tests\Unteist\Assert\Matcher
+ * @author Andrey Kolchenko <andrey@kolchenko.me>
+ */
+class EqualToTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return array
+     */
+    public function dpGoodWay()
+    {
+        return [
+            [5, 5],
+            [true, true],
+            [false, false],
+            ['str', 'str'],
+            [['a' => 'b', 'c' => 'd'], ['c' => 'd', 'a' => 'b']],
+            [0, 'string'],
+            [4.2, '4.2 str'],
+            [null, ''],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function dpBadWay()
+    {
+        return [
+            [-7, -3],
+            [0, 0.2],
+            ['str1', 'str2'],
+            [['a' => 'b', 'c' => 'd'], ['d' => 'c', 'b' => 'a']],
+            [null, 'f'],
+        ];
+    }
+
+    /**
+     * @param mixed $actual
+     * @param mixed $expected
+     *
+     * @dataProvider dpGoodWay
+     */
+    public function testGoodWay($actual, $expected)
+    {
+        $class = new EqualTo($expected);
+        $class->match($actual);
+    }
+
+    /**
+     * @param mixed $actual
+     * @param mixed $expected
+     *
+     * @dataProvider dpBadWay
+     * @expectedException \Unteist\Exception\TestFailException
+     * @expectedExceptionMessage Failed asserting that variables are equals:
+     */
+    public function testBadWay($actual, $expected)
+    {
+        $class = new EqualTo($expected);
+        $class->match($actual);
+    }
+}
