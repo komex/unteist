ea0ada31978926b06fdbd754db9fce701f0e1721
Andrey Kolchenko <komexx@gmail.com>
2013-09-05 17:31:51 +0400

    Test for dataProvider in TestMeta

---
+++ tests/Unteist/Meta/TestMetaTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is a part of Unteist project.
+ *
+ * (c) Andrey Kolchenko <andrey@kolchenko.me>
+ */
+
+namespace Tests\Unteist\Meta;
+
+use Monolog\Handler\NullHandler;
+use Monolog\Logger;
+use Unteist\Meta\TestMeta;
+
+/**
+ * Class TestMetaTest
+ *
+ * @package Tests\Unteist\Meta
+ * @author Andrey Kolchenko <andrey@kolchenko.me>
+ */
+class TestMetaTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Logger
+     */
+    protected static $logger;
+
+    /**
+     * Prepare case.
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$logger = new Logger('test');
+        self::$logger->pushHandler(new NullHandler());
+    }
+
+    /**
+     * @return array
+     */
+    public function dpDataProvider()
+    {
+        return [
+            [['dataProvider' => 'dp'], 'dp'],
+            [['dataProvider' => 'method']],
+            [['dataProvider' => true]],
+            [['dataProvider' => '']],
+            [['Dataprovider' => 'dp']],
+            [[]],
+        ];
+    }
+
+    /**
+     * @param array $modifiers
+     * @param string $expected
+     *
+     * @dataProvider dpDataProvider
+     */
+    public function testDataProvider(array $modifiers, $expected = null)
+    {
+        $meta = new TestMeta('class', 'method', $modifiers, self::$logger);
+        $this->assertEquals($expected, $meta->getDataProvider());
+    }
+}
